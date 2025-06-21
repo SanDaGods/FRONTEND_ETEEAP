c10e4f7e5ad7031a88c3502fe0916242ebe2d306
SanDaGods <christianjedhsandagan@gmail.com>
2025-06-22 03:40:15 +0800

    back

--- frontend/client/applicant/timeline/timeline.js
+++ frontend/client/applicant/timeline/timeline.js
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from '../../../shared/js/common/config.js';
+import { API_BASE_URL } from '/shared/js/common/config.js';
 
 document.addEventListener("DOMContentLoaded", function () {
       
