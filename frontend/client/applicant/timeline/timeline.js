99c71174b0f0c012cc9631ddab6d5f45251ece90
SanDaGods <christianjedhsandagan@gmail.com>
2025-06-21 21:09:43 +0800

    try

--- frontend/client/applicant/timeline/timeline.js
+++ frontend/client/applicant/timeline/timeline.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://backendeteeap-production.up.railway.app";
+import { API_BASE_URL } from '../../../shared/js/common/config.js';
 
 document.addEventListener("DOMContentLoaded", function () {
       
---
+++ frontend/client/shared/js/common/config.js
@@ -0,0 +1,1 @@
+export const API_BASE_URL = "https://backendeteeap-production.up.railway.app";
